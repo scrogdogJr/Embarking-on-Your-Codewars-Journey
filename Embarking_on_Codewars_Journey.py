b8e9eda367fb104ee1a615e3f4f6b545f8a131c0
Anthony Scroggins <anthony7101@cox.net>
2025-01-24 19:57:48 -0700

    All three codewars challenges

--- Embarking_on_Codewars_Journey.py
+++ Embarking_on_Codewars_Journey.py
@@ -1,3 +1,24 @@
-print("testing github")
+#Solution: Even or Odd
 
-print("another test")
+def even_or_odd(number):
+    
+    if (number % 2) == 0:
+        return "Even"
+    
+    else:
+        return "Odd"
+
+#Solution: Convert a number to a string
+
+def number_to_string(num):
+    num = str(num)
+    
+    return num
+
+def get_count(sentence):
+    count = 0
+    for char in sentence:
+        if (sentence[char] == "a") or (sentence[char] == "e") or (sentence[char] == "i") or (sentence[char] == "o") or (sentence[char] == "u"):
+            count += 1
+
+    return count;
